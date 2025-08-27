3430abd0e970025190f78fba058cdb9cd38e9148
JustinM1988 <Justin.mcintyre@portlandtx.gov>
2025-08-27 16:54:16 -0500

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-// 1) Set CLIENT_ID to the OAuth 2.0 App ID from your ArcGIS Developers dashboard
-// 2) If you’re using ArcGIS Enterprise, set PORTAL to your portal’s /sharing/rest URL
-//    e.g., "https://my-portal.domain.com/portal/sharing/rest"
-
-export const CONFIG = {
-  CLIENT_ID: "ic6BRtZvKEpNKVjS",
-  PORTAL: "https://www.arcgis.com/sharing/rest"
-};
