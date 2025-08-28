146fc23111895b12459a60932045590ee930a437
JustinM1988 <Justin.mcintyre@portlandtx.gov>
2025-08-28 09:27:10 -0500

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// config.js — Justin's repo; used for reference only (app already hardcodes these)
-window.CONFIG = {
-  CLIENT_ID: "ic6BRtzVkEpNKVjS",
-  PORTAL: "https://www.arcgis.com/sharing/rest"
-};
