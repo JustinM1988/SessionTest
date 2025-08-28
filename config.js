3febe16c0b0a0770cf308460882351fa7dcd0fd9
JustinM1988 <Justin.mcintyre@portlandtx.gov>
2025-08-28 08:24:06 -0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js — global CONFIG used by app.js & callback.js
-// Replace CLIENT_ID with your ArcGIS OAuth App ID.
-window.CONFIG = {
-  CLIENT_ID: "YOUR_CLIENT_ID_HERE",
-  PORTAL: "https://www.arcgis.com/sharing/rest"
-};
